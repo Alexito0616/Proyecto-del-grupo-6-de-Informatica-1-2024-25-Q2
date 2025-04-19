8c5048ff748b41f35b491a054ba7c86427d810f1
Alexito0616 <alex.garcia.gombau@estudiantat.upc.edu>
2025-04-19 20:40:58 +0200

    Delete Informatica directory

--- Informatica/BLO 1 -5.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Dime un número:"))
-b = int(input("Dime otro:"))
-c = int(input("Dime el ultimo:"))
-
-if a == b + c or b == a + c or c == a + b:
-    print("Se cumple")
-
-else:
-    print("No se cumple")
