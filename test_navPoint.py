a4f05c37e6f319ace66c661dd38e92a65e045f12
RogerMF33 <roger.monreal@estudiantat.upc.edu>
2025-05-28 19:51:28 +0200

    Update test_navPoint.py

--- test_navPoint.py
+++ test_navPoint.py
@@ -5,11 +5,13 @@ from navPoint import NavPoint
 class TestNavPoint(unittest.TestCase):
    def test_creation(self):
        np = NavPoint(6062, "IZA.A", 38.877280483, 1.3690495)
-       self.assertEqual(np.number, 6062)
+       self.assertEqual(np.id, 6062)
        self.assertEqual(np.name, "IZA.A")
-       self.assertAlmostEqual(np.latitude, 38.877280483)
-       self.assertAlmostEqual(np.longitude, 1.3690495)
+       self.assertAlmostEqual(np.lat, 38.877280483)
+       self.assertAlmostEqual(np.lon, 1.3690495)
 
 
 if __name__ == "__main__":
    unittest.main()
+
+
