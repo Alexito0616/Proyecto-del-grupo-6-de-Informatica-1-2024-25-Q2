b8f6778ffec4009e06ef130a37506719058b6c95
RogerMF33 <roger.monreal@estudiantat.upc.edu>
2025-05-28 19:51:40 +0200

    Update test_navSegment.py

--- test_navSegment.py
+++ test_navSegment.py
@@ -5,10 +5,14 @@ from navSegment import NavSegment
 class TestNavSegment(unittest.TestCase):
    def test_creation(self):
        seg = NavSegment(6063, 6062, 40.879793)
-       self.assertEqual(seg.origin_number, 6063)
-       self.assertEqual(seg.destination_number, 6062)
+       self.assertEqual(seg.origin_id, 6063)
+       self.assertEqual(seg.dest_id, 6062)
        self.assertAlmostEqual(seg.distance, 40.879793)
 
 
 if __name__ == "__main__":
    unittest.main()
+
+
+
+
